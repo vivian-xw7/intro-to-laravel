4230d9a3d9fcaf48b707a3ab14c0f374349a2fef
Vivian Hayes <viv.hayes@yahoo.com>
2021-08-28 18:58:59 -0500

    Create factories for Category and Post.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,40 +21,49 @@ class DatabaseSeeder extends Seeder
         Post::truncate();
         Category::truncate();
 
-        $users = User::factory(3)->create();
-
-        // this is for testing and local development only
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
+        // To control the name
+        $user = User::factory()->create([
+            'name' => 'Battling Bard'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family'
+        Post::factory(5)->create([
+            'user_id' => $user->first()->id
         ]);
 
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
+        // $users = User::factory(3)->create();
 
-        Post::create([
-            'user_id' => $users->first()->id,
-            'category_id' => $family->id,
-            'title' => 'My Family Post',
-            'slug' => 'my-family-post',
-            'excerpt' => '<p>Excerpt of post.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
-        ]);
+        // // this is for testing and local development only
+        // $personal = Category::create([
+        //     'name' => 'Personal',
+        //     'slug' => 'personal'
+        // ]);
 
-        Post::create([
-            'user_id' => $users->first()->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post',
-            'slug' => 'my-work-post',
-            'excerpt' => '<p>Excerpt of post.</p>',
-            'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
-        ]);
+        // $family = Category::create([
+        //     'name' => 'Family',
+        //     'slug' => 'family'
+        // ]);
+
+        // $work = Category::create([
+        //     'name' => 'Work',
+        //     'slug' => 'work'
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $users->first()->id,
+        //     'category_id' => $family->id,
+        //     'title' => 'My Family Post',
+        //     'slug' => 'my-family-post',
+        //     'excerpt' => '<p>Excerpt of post.</p>',
+        //     'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+        // ]);
+
+        // Post::create([
+        //     'user_id' => $users->first()->id,
+        //     'category_id' => $work->id,
+        //     'title' => 'My Work Post',
+        //     'slug' => 'my-work-post',
+        //     'excerpt' => '<p>Excerpt of post.</p>',
+        //     'body' => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'
+        // ]);
     }
 }
