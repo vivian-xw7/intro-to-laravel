9937ccacdb1f54a723204cbd7a467371fbc1b81d
Vivian Hayes <viv.hayes@yahoo.com>
2021-09-01 13:16:49 -0500

    Make the post and category button dynamic.

--- resources/views/components/post-featured-card.blade.php
+++ resources/views/components/post-featured-card.blade.php
@@ -41,7 +41,6 @@
                     <img src="/images/lary-avatar.svg" alt="Lary avatar">
                     <div class="ml-3">
                         <h5 class="font-bold">{{ $post->user->name }}</h5>
-                        <h6>Mascot at Laracasts</h6>
                     </div>
                 </div>
 
