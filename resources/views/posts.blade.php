876e65e3a335c6525de5e6c58dfcccb7cb4869e8
Vivian Hayes <viv.hayes@yahoo.com>
2021-09-01 10:59:00 -0500

    Make post-car and post-featured-card more dynamic.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,22 +5,28 @@
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
 
-        <x-post-featured-card />
+        <x-post-featured-card :post="$posts[0]" />
 
         <div class="lg:grid lg:grid-cols-2">
 
-            <x-post-card />
-            <x-post-card />
+            @foreach ($posts->skip(1) as $post)
+
+                <x-post-card :post="$post" />
+
+            @endforeach
+
+
+            <!-- <x-post-card /> -->
 
         </div>
 
-        <div class="lg:grid lg:grid-cols-3">
+        <!-- <div class="lg:grid lg:grid-cols-3">
             
             <x-post-card />
             <x-post-card />
             <x-post-card />
 
-        </div>
+        </div> -->
     </main>
 
 
