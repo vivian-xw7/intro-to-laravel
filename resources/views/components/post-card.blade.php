eec8b6923634a65d55001dc67f4b2d780171b180
Vivian Hayes <viv.hayes@yahoo.com>
2021-09-01 12:48:54 -0500

    Move the CSS posts grid into its own component.

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,7 +1,8 @@
 @props(['post'])
 
 <article
-    class="transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl">
+    {{ $attributes->merge(['class' => "transition-colors duration-300 hover:bg-gray-100 border border-black border-opacity-0 hover:border-opacity-5 rounded-xl"])}}
+>
     <div class="py-6 px-5">
         <div>
             {{--TODO--}}
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,56 +5,17 @@
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
 
-        <x-post-featured-card :post="$posts[0]" />
+        @if ($posts->count())
 
-        <div class="lg:grid lg:grid-cols-2">
+            <x-posts-grid :posts="$posts" />
 
-            @foreach ($posts->skip(1) as $post)
+        @else
 
-                <x-post-card :post="$post" />
+            <p class="text-center">No posts yet. Please check again later.</p>
 
-            @endforeach
+        @endif
 
-
-            <!-- <x-post-card /> -->
-
-        </div>
-
-        <!-- <div class="lg:grid lg:grid-cols-3">
-            
-            <x-post-card />
-            <x-post-card />
-            <x-post-card />
-
-        </div> -->
     </main>
-
-
-
-    <!-- @foreach ($posts as $post)
-
-        <article>
-
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {{ $post->title }}
-                </a>
-            </h1>
-
-            <p>
-            By <a href="">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
-
-
-            <div>
-                {{ $post->exerpt }}
-            </div>
-
-        </article>
-
-    @endforeach -->
-
-
 
 </x-layout>
 
