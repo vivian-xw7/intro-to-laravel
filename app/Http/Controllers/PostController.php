029cc439b230d9f0944a8ede458c055826cde5e4
Vivian Hayes <vivian.xw7@gmail.com>
2021-09-11 23:44:44 -0500

    Paginate.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,13 +20,20 @@ class PostController extends Controller
         //         ->orWhere('body', 'like' , '%' . request('search') . '%');
         // }
 
+        // return view('posts', [
+        //     // latest() orders them with the most recent at the top
+        //     // 'posts' => $posts->get(),
+        //     'posts' => Post::latest()->with('category')->filter(request(['search', 'category', 'user']))->get(),
+
+        //     'categories' => Category::all(),
+        //     'currentCategory' => Category::where('slug', request('category'))->first()
+        // ]);
+
         return view('posts', [
-            // latest() orders them with the most recent at the top
-            // 'posts' => $posts->get(),
             'posts' => Post::latest()->with('category')->filter(request(['search', 'category', 'user']))->get(),
 
             'categories' => Category::all(),
-            'currentCategory' => Category::where('slug', request('category'))->first()
+            'currentCategory' => Category::where('slug', request('category'))->paginate(6)
         ]);
     }
     
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -11,6 +11,8 @@
 
             <x-posts-grid :posts="$posts" />
 
+            {{ $posts->links() }}
+
         @else
 
             <p class="text-center">No posts yet. Please check again later.</p>
