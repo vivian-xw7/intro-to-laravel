b70ea2b8d7a20e0e9a7be570e6b6b818ee77e07e
Vivian Hayes <vivian.xw7@gmail.com>
2021-09-18 17:38:53 -0500

    Make login page.

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,62 +1,15 @@
 
-
 <x-layout>
 
     <section class="px-6 py-8">
 
         <main class="max-w-lg mx-auto mt-10 bg-gray-100 border-gray-200 p-6 rounded-xl">
 
-            <h1 class="text-center font-bold text-xl">Register!</h1>
+            <h1 class="text-center font-bold text-xl">Log In!</h1>
 
-            <form method="POST" action="/register" class="mt-10">
+            <form method="POST" action="/login" class="mt-10"> {{-- The action endpoint needs to match the route endpoint --}}
 
                 @csrf
-
-                <div class="mb-6">
-                    <label 
-                        class="block mb-2 uppercase font-bold text-xs text-gray-700" 
-                        for="name"
-                    >
-                        Name
-                    </label>
-
-                    <input 
-                        class="border border-gray-400 p-2 w-full" 
-                        type="text" 
-                        name="name" 
-                        id="name" 
-                        value="{{ old('name') }}"
-                        required
-                    > 
-                    
-                    @error('name')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-
-                </div>
-
-                <div class="mb-6">
-                    <label 
-                        class="block mb-2 uppercase font-bold text-xs text-gray-700" 
-                        for="username"
-                    >
-                        Username
-                    </label>
-
-                    <input 
-                        class="border border-gray-400 p-2 w-full" 
-                        type="text" 
-                        name="username" 
-                        id="username" 
-                        value="{{ old('username') }}"
-                        required
-                    >
-                    
-                    @error('username')
-                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-
-                </div>
 
                 <div class="mb-6">
                     <label 
@@ -107,7 +60,7 @@
                     <button type="submit"
                             class="bg-blue-400 text-white rounded py-2 hover:bg-blue-500"
                     >
-                        Register
+                        Login
                     </button>
                 </div>
 
@@ -126,3 +79,4 @@
     </section>
 
 </x-layout>
+
--- routes/web.php
+++ routes/web.php
@@ -32,5 +32,9 @@ Route::get('register', [RegisterController::class, 'create'])->middleware('guest
 Route::post('register', [RegisterController::class, 'store'])->middleware('guest');
 
 
-Route::post('logout', [SessionsController::class, 'destroy']);
+Route::get('login', [SessionsController::class, 'create'])->middleware('guest');
+
+Route::post('login', [SessionsController::class, 'store'])->middleware('guest');
+
+Route::post('logout', [SessionsController::class, 'destroy'])->middleware('auth');
 
