9a81db2d1340d2eb986058e3cd4a89e96b42b83b
Vivian Hayes <vivian.xw7@gmail.com>
2021-10-07 23:58:28 -0500

    Extract form elements into components.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -14,43 +14,10 @@
 
                 <x-form.input name="thumbnail" type="file" />
     
-                
+                <x-form.textarea name="excerpt" />
 
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" 
-                        for="excerpt"
-                    >
-                        Excerpt
-                    </label>
-    
-                    <textarea class="border border-gray-400 p-2 w-full" 
-                        name="excerpt" 
-                        id="excerpt" 
-                        required>{{ old('excerpt') }}</textarea>
-    
-                    @error('excerpt')
-                        <p class="text-red-500 text-xs mt-2">{{ message }}</p>
-                    @enderror
-    
-                </div>
+                <x-form.textarea name="body" />
 
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" 
-                        for="body"
-                    >
-                        Body
-                    </label>
-    
-                    <textarea class="border border-gray-400 p-2 w-full" 
-                        name="body" 
-                        id="body" 
-                        required>{{ old('body') }}</textarea>
-    
-                    @error('body')
-                        <p class="text-red-500 text-xs mt-2">{{ message }}</p>
-                    @enderror
-    
-                </div>
 
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" 
