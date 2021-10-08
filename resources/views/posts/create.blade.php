7131e233f5208235fcb5a872a62ddae999f7f7aa
Vivian Hayes <vivian.xw7@gmail.com>
2021-10-07 23:52:30 -0500

    Make input form more dynamic.

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,67 +7,14 @@
         <x-panel class="">
             <form method="POST" action="/admin/posts" enctype="multipart/form-data">
                 @csrf
-    
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" 
-                        for="title"
-                    >
-                        Title
-                    </label>
-    
-                    <input class="border border-gray-400 p-2 w-full" 
-                        type="text" 
-                        name="title" 
-                        value="{{ old('title') }}"
-                        id="title" 
-                        required
-                    >
-    
-                    @error('title')
-                        <p class="text-red-500 text-xs mt-2">{{ message }}</p>
-                    @enderror
-    
-                </div>
 
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" 
-                        for="slug"
-                    >
-                        Slug
-                    </label>
-    
-                    <input class="border border-gray-400 p-2 w-full" 
-                        type="text" 
-                        name="slug" 
-                        id="slug" 
-                        required
-                    >
-    
-                    @error('slug')
-                        <p class="text-red-500 text-xs mt-2">{{ message }}</p>
-                    @enderror
-    
-                </div>
+                <x-form.input name="title" />
 
-                <div class="mb-6">
-                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" 
-                        for="thumbnail"
-                    >
-                        Thumbnail
-                    </label>
+                <x-form.input name="slug" />
+
+                <x-form.input name="thumbnail" type="file" />
     
-                    <input class="border border-gray-400 p-2 w-full" 
-                        type="file" 
-                        name="thumbnail" 
-                        id="thumbnail" 
-                        required
-                    >
-    
-                    @error('thumbnail')
-                        <p class="text-red-500 text-xs mt-2">{{ message }}</p>
-                    @enderror
-    
-                </div>
+                
 
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" 
