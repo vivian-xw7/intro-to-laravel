2bd62d6b7f9147bde3c51efe6fde122d2a196414
Vivian Hayes <vivian.xw7@gmail.com>
2021-10-10 23:58:35 -0500

    Create and 'edit' view for admin.

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -41,4 +41,9 @@ class AdminPostController extends Controller
 
         return redirect('/');
     }
+
+    public function edit (Post $post)
+    {
+        return view('admin.posts.edit', ['post' => $post]);
+    }
 }
